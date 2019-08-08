919f0f046fb7213f761fa02c5c1102bf2a1ab77c
liufei <2317741837@qq.com>
2019-08-08 20:31:30 +0800

    用户表mapper

---
+++ src/main/java/cn/wildfirechat/app/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package cn.wildfirechat.app.mapper;
+
+import cn.wildfirechat.app.model.User;
+
+public interface UserMapper {
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKeyWithBLOBs(User record);
+
+    int updateByPrimaryKey(User record);
+
+    User selectByMobile(String mobile);
+}
---
+++ src/main/java/cn/wildfirechat/app/model/User.java
@@ -0,0 +1,175 @@
+package cn.wildfirechat.app.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String uid;
+
+    private String name;
+
+    private String displayName;
+
+    private Integer gender;
+
+    private String portrait;
+
+    private String mobile;
+
+    private String email;
+
+    private String address;
+
+    private String company;
+
+    private String social;
+
+    private String passwdMd5;
+
+    private String salt;
+
+    private Byte type;
+
+    private Long dt;
+
+    private Date createtime;
+
+    private String extra;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName == null ? null : displayName.trim();
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getPortrait() {
+        return portrait;
+    }
+
+    public void setPortrait(String portrait) {
+        this.portrait = portrait == null ? null : portrait.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company == null ? null : company.trim();
+    }
+
+    public String getSocial() {
+        return social;
+    }
+
+    public void setSocial(String social) {
+        this.social = social == null ? null : social.trim();
+    }
+
+    public String getPasswdMd5() {
+        return passwdMd5;
+    }
+
+    public void setPasswdMd5(String passwdMd5) {
+        this.passwdMd5 = passwdMd5 == null ? null : passwdMd5.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt == null ? null : salt.trim();
+    }
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
+
+    public Long getDt() {
+        return dt;
+    }
+
+    public void setDt(Long dt) {
+        this.dt = dt;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra == null ? null : extra.trim();
+    }
+}
